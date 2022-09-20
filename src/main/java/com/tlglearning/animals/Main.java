c3faccfac707b3fcc2c3828a03b9f3b545382657
gmejia3 <gemejiajr@gmail.com>
2022-09-20 10:06:28 -0700

    Initialization demonstration

--- src/main/java/com/tlglearning/animals/Main.java
+++ src/main/java/com/tlglearning/animals/Main.java
@@ -1,31 +1,17 @@
 package com.tlglearning.animals;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class Main {
 
   public static void main(String[] args) {
-    List<Wolf> wolves = new ArrayList<>();
-    wolves.add(new Wolf());
-    wolves.add(new Wolf());
-    wolves.add(new Wolf());
-    wolves.add(new Dog());
-    wolves.add(new Dog());
-    wolves.add(new Dog());
-    Collections.shuffle(wolves);
-    for (Wolf w : wolves) {
-      System.out.println();
-      System.out.println(w.getClass().getName());
-      w.hunt();
-      w.vocalize();
-      if (w instanceof Dog) {
-//        ((Dog) w).rollOver();
-        Dog d = (Dog) w;
-        d.rollOver();
-      }
-    }
+   new Dog();
+   /*
+   0. Class initialization of Wolf superclass (Object) - not necessary, because Object is already loaded and initialized.
+   1. Class initialization of wolf
+   2. Class initialization of dog
+   (2a). Object's initialization of Dog instance.
+   3. Wolf's initialization of dog
+   4. Dog's initialization of dog
+    */
   }
 }
 
--- src/main/java/com/tlglearning/animals/Wolf.java
+++ src/main/java/com/tlglearning/animals/Wolf.java
@@ -2,6 +2,31 @@ package com.tlglearning.animals;
 
 public class Wolf {
 
+  private static final int MY_CONSTANT = initializeValue();
+
+  private int data = initializeData();
+
+  static {
+    System.out.println("Wolf static initialization block");
+  }
+
+  {
+    System.out.println("Wolf instance initialization block");
+  }
+
+  public Wolf() {
+    System.out.println("Wolf constructor");
+  }
+  private static int initializeValue() {
+    System.out.println("Wolf.initializeValue");
+    return -1;
+  }
+
+  private int initializeData() {
+    System.out.println("Wold.initializeData");
+    return 1;
+  }
+
   public void vocalize() {
     System.out.println("Howl like a wolf!");
   }
